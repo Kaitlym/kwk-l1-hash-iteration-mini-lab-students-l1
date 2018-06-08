299bd1b988bd279747f3f9db792afd38b9a80027
Kaitlym <azkatie2014@gmail.com>
2018-06-08 18:36:53 +0000

    automatically backed up by learn

---
+++ hashes.rb
@@ -0,0 +1,24 @@
+# hash = {"cheese" => "a yummy dairy snack"}
+
+# chara_traits=["neat freak", "jokester", "free spirit"]
+# chara_name=["monica", "joey", "phobe"]
+
+# chara_friends = {}
+
+# index = 0
+# chara_traits.each do |name|
+#   chara_friends[name] = chara_traits[index]
+#   index += 1
+# end
+# puts chara_friends
+
+celeb_friend={} 
+celeb_friend["kaitlyn"] = "hermonie granger"
+
+class_celeb_friend{
+  "jack" => "gerard way",
+  "gerardo" => "wolf",
+  "fidel" => "the mafia"
+}
+puts class_celeb_friend.values
+puts class_celeb_friend.key
--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,16 +1,41 @@
-
 def create_olympics_hash
   # Implement this method so that it returns a hash with the data provided on README.md
+  summer_olympics = {
+    :sydney => 2000,
+    :athens => 2004,
+    :london => 2012
+  }
+  summer_olympics[:atlanta] = 1996
+  puts summer_olympics
 end
 
 def add_a_key_value_pair
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
+   summer_olympics = {
+    :sydney => 2000,
+    :athens => 2004,
+    :beijing => 2008,
+    :london => 2012
+  }
+  summer_olympics[:atlanta] = 1996
+  puts summer_olympics
 end
 
 def iterate_through_hash
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
   # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
+  summer_olympics = {
+    :sydney => 2000,
+    :athens => 2004,
+    :beijing => 2008,
+    :london => 2012
+  }
+  summer_olympics[:atlanta] = 1996
+  summer_olympics.each do |place, year|
+    puts "The #{place} summer olympics took place in #{year}."
+  end
 end
+iterate_through_hash
 
 def iterate_through_keys
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
